c1e4bcb6cfe88442b3b2ef188440660e689e0e22
ngotuankiet1 <tuankiet281299@gmail.com>
2021-12-19 00:28:37 +0700

    chia file router and config server ra rieng

--- Dutchie-server/backend/index.js
+++ Dutchie-server/backend/index.js
@@ -1,29 +1,9 @@
-const mysql = require("mysql")
 const express = require("express")
-const connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    user: 'root',
-    password: 'tob1',
-    database: 'DB_DUTCHIE_V1',
-    port: 3306
-});
+const router = require('./router/router')
 
-const  app = express();
+const  app = express()
 
-app.get('/', ((req, res) => {
-    connection.query("SELECT * FROM EMPLOYEE", (err, rows) => {
-        if(err) {
-            res.json({
-                success: false,
-                err
-            })
-        } else {
-            res.json({
-                success: true,
-                rows
-            })
-        }
-    })
-}))
+app.use('/',router)
+
 
 app.listen(3000,()=> console.log("listeing to 3000"))
